5f4017f9ae2a523d47d6ffc46a6cc521ae9f3228
ieyeppo <ieyeppo@gmail.com>
2021-03-22 12:57:19 +0900

    커밋

---
+++ WPFApp/WpfPracticeApp/Product.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using WpfPracticeApp.BusinessLogic;
+
+namespace WpfPracticeApp
+{
+    /// <summary>
+    /// Product.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Product : Page
+    {
+        public Product()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Initialized(object sender, EventArgs e)
+        {
+            var cars = new List<BusinessLogic.Car>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                cars.Add(new BusinessLogic.Car() { Speed = i * 10 });
+            }
+
+            this.DataContext = cars;
+        }
+    }
+}
