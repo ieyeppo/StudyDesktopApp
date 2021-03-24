6c9e6c7c341a098628f4e407c126f877e84a7395
ieyeppo <ieyeppo@gmail.com>
2021-03-24 12:39:30 +0900

    커밋

---
+++ WPFApp/WpfTextApp/PersonList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTextApp
+{
+    public class PersonList : ObservableCollection<Person>
+    {
+        public PersonList()
+        {
+            this.Add(new Person("Willa", "Cather"));
+            this.Add(new Person("Isak", "Dinesen"));
+            this.Add(new Person("최", "혀나"));
+            this.Add(new Person("최", "콩지"));
+        }
+    }
+}
